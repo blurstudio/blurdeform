87a3017adb9d5524ca05ba72623e6c2d8598cd9c
Tyler Fox <tyler@blur.com>
2022-05-11 16:08:12 -0700

    Move to a consistent cmake setup

---
+++ mayaConfigure.bat
@@ -0,0 +1,19 @@
+setlocal
+
+SET MAYA_VERSION=2023
+SET BUILD=mayabuild_%MAYA_VERSION%
+SET COMPILER=Visual Studio 16 2019
+
+SET PFX=%~dp0
+cd %PFX%
+rmdir %BUILD% /s /q
+mkdir %BUILD%
+cd %BUILD%
+
+cmake ^
+    -DMAYA_VERSION=%MAYA_VERSION% ^
+    -G "%COMPILER%" ..\
+
+cmake --build . --config Release --target INSTALL
+
+pause
