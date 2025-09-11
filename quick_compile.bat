e285dade3da497706e99a0d6ee08d4fa921f79b8
Tyler Fox <tyler@blur.com>
2025-09-10 17:34:35 -0700

    Update the maya subproject, and setup compiling for maya 2026

--- quick_compile.bat
+++ quick_compile.bat
@@ -1,6 +1,6 @@
 setlocal
 
-SET MAYA_VERSION=2024
+SET MAYA_VERSION=2026
 REM "vs" "ninja"
 REM use VS for the debugger, otherwise use NINJA
 REM Until I figure out how to debug using nvim
@@ -10,12 +10,15 @@ SET BUILDTYPE=release
 SET BUILDDIR=mayabuild_%BUILDTYPE%_%MAYA_VERSION%_%BACKEND%
 
 if not exist %BUILDDIR%\ (
-    meson setup -Dmaya:maya_version=%MAYA_VERSION% --buildtype %BUILDTYPE% --vsenv %BUILDDIR% --backend %BACKEND%
+    meson setup %BUILDDIR% ^
+    -Dmaya:maya_version=%MAYA_VERSION% ^
+    -Dmaya:maya_devkit_base=D:\Autodesk\MayaDev\Maya2026_2\devkitBase ^
+    --buildtype %BUILDTYPE% --vsenv --backend %BACKEND%
 )
 
 if exist %BUILDDIR%\ (
     meson compile -C %BUILDDIR%
-    meson install -C %BUILDDIR%
+    meson install --skip-subprojects -C %BUILDDIR%
 )
 
 pause
